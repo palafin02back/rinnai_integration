39b3e801e7fc832d09401c68a3fb5cb5f0e6119e
palafin02back <palafin@gear4ai.com>
2025-05-19 08:05:19 +0000

    fix config flow on older version

--- custom_components/rinnai/config_flow.py
+++ custom_components/rinnai/config_flow.py
@@ -109,7 +109,8 @@ class RinnaiOptionsFlowHandler(OptionsFlow):
 
     def __init__(self, config_entry: ConfigEntry) -> None:
         """Initialize options flow."""
-        super().__init__(config_entry)
+        super().__init__()
+        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
